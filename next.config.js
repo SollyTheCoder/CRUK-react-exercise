2c595826a37e7ed765abee0174568b69cc0ca0a1
solomon <solomonowenwilliams@gmail.com>
2026-05-18 14:02:05 +0100

    upgrade packages
    nextjs 16, cruk-react-components v7

--- next.config.js
+++ next.config.js
@@ -1,25 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // minify is true by default in nextjs 16
+  // swcMinify: true,
 
   compiler: {
     // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-    styledComponents: {
-      // fileName?: boolean,
-      // Empty by default.
-      // topLevelImportPaths?: string[],
-      // Defaults to ["index"].
-      // meaninglessFileNames?: string[],
-      // Empty by default.
-      // namespace?: string,
-      // Not supported yet.
-      // minify?: boolean,
-      // Not supported yet.
-      // transpileTemplateLiterals?: boolean,
-      // Not supported yet.
-      // pure?: boolean,
-    },
+    // styledComponents: true,
+    //{
+    // fileName?: boolean,
+    // Empty by default.
+    // topLevelImportPaths?: string[],
+    // Defaults to ["index"].
+    // meaninglessFileNames?: string[],
+    // Empty by default.
+    // namespace?: string,
+    // Not supported yet.
+    // minify?: boolean,
+    // Not supported yet.
+    // transpileTemplateLiterals?: boolean,
+    // Not supported yet.
+    // pure?: boolean,
   },
 };
 
